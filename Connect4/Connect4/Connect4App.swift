87c04813f56bf8d5a2af1f8220271b30ad9f6e1c
Mark Carruthers <markpc17@sky.com>
2021-04-05 20:59:49 +0100

    Created SwiftUI Connect4 project for macOS

---
+++ Connect4/Connect4/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Connect4
+//
+//  Created by Mark Carruthers on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect4/Connect4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by Mark Carruthers on 05/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
